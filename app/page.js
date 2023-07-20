62dc41898677fc6a810de412bf47d525dd9adae2
Michelle Farley <shellfarley@gmail.com>
2023-07-20 13:15:15 +1000

    update small screen breakpoint

--- app/page.js
+++ app/page.js
@@ -23,7 +23,7 @@ const Main = styled.main`
     margin: 0 auto;
   }
 
-  @media (max-width: 560px) {
+  @media (max-width: 720px) {
     display: grid;
     grid-template-columns: 1fr;
     gap: 1rem;
@@ -40,7 +40,7 @@ const TitleCard = styled.div`
   grid-row-end: 3;
   margin-top: 0;
 
-  @media (max-width: 960px) and (min-width: 560px) {
+  @media (max-width: 960px) and (min-width: 721px) {
     grid-column-start: 1;
     grid-column-end: 3;
   }
